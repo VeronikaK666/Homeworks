5d6463b88492840572757e9f76c40e2cc4f436fa
VeronikaK666 <168474195+VeronikaK666@users.noreply.github.com>
2024-05-12 01:42:48 +0300

    Add files via upload

---
+++ Cycle_for.py
@@ -0,0 +1,9 @@
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+for i in range(len(cars)):
+    cars_count += 10
+    print("Я езжу на автомобиле марки ", cars[i])
+
+    print(cars_count)
+
+
