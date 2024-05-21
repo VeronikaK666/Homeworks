a69c87711be0041ee0c972dd6bd580cc4f760b45
VeronikaK666 <168474195+VeronikaK666@users.noreply.github.com>
2024-05-21 22:22:37 +0300

    Add files via upload

---
+++ mod1_hard.py
@@ -0,0 +1,8 @@
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+grades=[[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = list(students)
+students = sorted(students)
+average = [sum(grades[0]) / len(grades[0]), sum(grades[1]) / len(grades[1]), sum(grades[2]) / len(grades[2]),
+           sum(grades[3]) / len(grades[3]), sum(grades[4]) / len(grades[4])]
+average_grade = dict(zip(students, average))
+print(average_grade)
