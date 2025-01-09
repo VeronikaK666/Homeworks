ffe3d4ac1a8c7c479e7b4bc98fba76c85c665024
VeronikaK666 <168474195+VeronikaK666@users.noreply.github.com>
2025-01-09 23:47:18 +0300

    Update mod9_1hw.py

--- mod9_1hw.py
+++ mod9_1hw.py
@@ -4,15 +4,15 @@
 def apply_all_func(int_list, *functions):
     # В функции apply_all_func создайте пустой словарь results.
     results = {}
-    # # Переберите все функции из *functions.
+    # Переберите все функции из *functions.
     for func in functions:
         try:
             # Функция apply_all_func(int_list, *functions) должна:
             # вызвать каждую функцию к переданному списку int_list
-            # # При переборе функций записывайте в словарь results результат работы этой функции под ключом её названия.
+            # При переборе функций записывайте в словарь results результат работы этой функции под ключом её названия.
             results[func.__name__] = func(int_list)
         except Exception as e:
-            #  # Исключение может возникнуть, если функция не предназначена для работы с переданным типом данных.
+            # Исключение может возникнуть, если функция не предназначена для работы с переданным типом данных.
             results[func.__name__] = str(e)
     # Верните словарь results.
     # Функция apply_all_func(int_list, *functions) должна:
@@ -23,5 +23,4 @@ def apply_all_func(int_list, *functions):
 # Запустите функцию apply_all_func, передав в неё список из чисел и набор других функций.
 # Пример результата выполнения программы:
 print(apply_all_func([6, 20, 15, 9], max, min))
-#
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
